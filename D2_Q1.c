7dd63511793c5ca82d2a818fc5403c7ede56ccea
Vrinda Kumar <94823422+vrinda-kumar@users.noreply.github.com>
2026-02-04 19:08:54 +0530

    Add files via upload

---
+++ D2_Q1.c
@@ -0,0 +1,27 @@
+// Write a C program to delete the element at a given 1-based position pos from an array of n integers. Shift remaining elements to the left.
+#include <stdio.h>
+
+int main() {
+    int n, pos;
+    int a[100];
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    scanf("%d", &pos);
+    
+    for (int i = pos - 1; i < n - 1; i++) {
+        a[i] = a[i + 1];
+    }
+    
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d", a[i]);
+        if (i < n - 2) printf(" ");
+    }
+    printf("\n");
+    
+    return 0;
+}
